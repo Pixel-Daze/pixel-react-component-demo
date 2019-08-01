29fed9e33b0535397cf8665cfe092cbf4c9eb5cc
pixel-daze <1226093323@qq.com>
2019-08-01 17:13:51 +0800

    tiny success

---
+++ src/Tag.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Tag extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    render () {
+        return <div>
+            <span>hello world</span>
+        </div>
+    }
+}
+
+export default Tag;
---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Tag from './Tag';
+
+ReactDOM.render(<Tag />, document.getElementById('app'));
